c317a845af02212dbe6b0fa363106f5262151502
Gus <augustin.burchell@gmail.com>
2020-12-21 13:39:43 -0600

    Initial work on the tile component for a single book

---
+++ src/components/BookTile.svelte
@@ -0,0 +1,85 @@
+<script type="text/javascript">
+  export let book;
+
+  let pagesRead = book.pagesRead;
+  $: progressPercent = Math.floor(pagesRead / book.totalPages * 100);
+  $: progressPercentStr = progressPercent + '%';
+  $: console.log(progressPercentStr);
+</script>
+
+
+<style type="text/css" media="screen">
+ .book-tile-flex {
+     /* width: 100%;
+        height: 100%; */
+     display: grid;
+     grid-template-columns: 1fr 3fr  1fr;
+     grid-template-rows: 2fr, 1fr;
+
+     grid-template-areas:
+         " id  blurb  rating "
+         " id  progress  rating "
+ }
+
+.booktitle {
+    grid-area: id;
+    color: DimGrey;
+    background-color: DarkSeaGreen;
+    padding: 1em;
+    border-radius: 0 2em 0 0;
+ }
+
+.booktitle > h2 {
+    font-size: 3em;
+ }
+
+.booktitle > h3 {
+    font-size: 1em;
+ }
+
+.booksummary {
+    grid-area: blurb;
+    padding: 1em;
+    text-align: center;
+ }
+
+ .progressbar {
+     grid-area: progress;
+     display: flex;
+     padding: 0.5em;
+ }
+
+ .total-bar {
+     background-color: LightGrey;
+     width: 100%;
+     text-align: center;
+ }
+
+ .completed-bar {
+     text-align: center;
+     height: 100%;
+     background-color: GoldenRod;
+     overflow: hidden;
+     max-width: 100%;
+     /* width: {progressPercentStr}; */
+ }
+
+ .pages-box {
+     margin-left: 3%;
+ }
+</style>
+
+
+<div class="book-tile-flex">
+    <div class="booktitle">
+        <h3>{book.author}</h3>
+        <h2>{book.title}</h2>
+    </div>
+    <div class="booksummary">
+        {book.summary}
+    </div>
+    <div class="progressbar">
+        <div class="total-bar"> <div class="completed-bar" style="width: {progressPercentStr}"> </div> </div>
+        <input class="pages-box" type=number bind:value={pagesRead} min=0 max={book.totalPages} />
+    </div>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,54 @@
+<script>
+	import successkid from 'images/successkid.jpg';
+	import BookTile from '../components/BookTile.svelte';
+ export let book = {
+	 title: "Foundation",
+	 author: "Isaac Asimov",
+	 summary: "The galactic empire is crumbling. Will 'scientific' models of the future help humanity rebuild?",
+	 pagesRead: 35,
+	 totalPages: 285,
+ };
+</script>
+
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Year of the word, ya heard.</title>
+</svelte:head>
+
+<h1>What've you been reading?</h1>
+
+<BookTile {book}/>
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
